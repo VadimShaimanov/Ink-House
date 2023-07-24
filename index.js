1b9e572cbca29f3678e2c26a4d08cb22ff7d2fcd
Vadim <vadim.shaimanov@gmail.com>
2023-07-24 15:51:16 +0300

    add_all

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ tabsBtn.forEach(function(element){
     document.querySelector(`[data-target="${path}"]`).classList.add('paintings__grid--active');
   });
 });
+
+
+document.querySelector(".header__btn-open").addEventListener("click", function() {
+  document.querySelector(".header__nav").classList.add("active");
+})
+document.querySelector(".nav__close").addEventListener("click", function() {
+  document.querySelector(".header__nav").classList.remove("active");
+})
